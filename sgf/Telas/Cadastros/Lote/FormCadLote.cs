1f5c3a5667bdcb903d734419c7c9420f79351eb0
Fernando Costa Colares <fcc281002@gmail.com>
2024-10-02 17:25:14 -0300

    commit27

---
+++ sgf/Telas/Cadastros/Lote/FormCadLote.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sgf.Telas.Cadastros.Lote
+{
+    public partial class FormCadLote : Form
+    {
+        public FormCadLote()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
