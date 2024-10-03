1dbc1b5d91de9c2530061a0d1c535ecdd4285899
Fernando Costa Colares <fcc281002@gmail.com>
2024-10-03 15:01:22 -0300

    commit30

---
+++ sgf/Controle/Lote.cs
@@ -0,0 +1,53 @@
+﻿using MySql.Data.MySqlClient;
+using sgf.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sgf.Controle
+{
+    internal class Lote
+    {
+        public int Id { get; set; }
+        public string Code_Lote { get; set; }
+        public string Fornecedor { get; set; }
+        public string Vencimento { get; set; }
+        public string Status { get; set; }
+
+        public void Lote_id(int id, string code_Lote, string fornecedor, string vencimento, string status)
+        {
+            Id = id;
+            Code_Lote = code_Lote;
+            Fornecedor = fornecedor;
+            Vencimento = vencimento;
+            Status = status;
+        }
+
+        public Lote(string code_Lote, string fornecedor, string vencimento, string status)
+        {
+            Code_Lote = code_Lote;
+            Fornecedor = fornecedor;
+            Vencimento = vencimento;
+            Status = status;
+        }
+
+        public static void SalvarLote(Lote Lote) 
+        {
+            using (MySqlConnection connection = new MySqlConnection(DBConnection.GetConnectionString()))
+            {
+                string query = "INSERT INTO Lote (code_lote, dt_validade_lote, status_lote, farmaceutica_lote) VALUES (@code_lote, @vencimento, @status, @fornecedor)";
+
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.Parameters.AddWithValue("@code_lote", Lote.Code_Lote);
+                command.Parameters.AddWithValue("@vencimento", Lote.Vencimento);
+                command.Parameters.AddWithValue("@status", Lote.Status);
+                command.Parameters.AddWithValue("@fornecedor", Lote.Fornecedor);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
