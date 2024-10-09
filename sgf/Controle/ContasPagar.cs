32fd73f6e0f006d032dd5cdc983f0b9a42a7b295
Fernando Costa Colares <fcc281002@gmail.com>
2024-10-09 11:04:57 -0300

    commit60

---
+++ sgf/Controle/ContasPagar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sgf.Controle
+{
+    internal class ContasPagar
+    {
+    }
+}
