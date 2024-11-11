bd35e4560dd3d15d07c3e7e98c7c7fd73198599a
Fernando Costa Colares <fcc281002@gmail.com>
2024-11-11 14:15:27 -0300

    commit81

--- sgf/Telas/Relatorios/FormRelatorioCompra.Designer.cs
+++ sgf/Telas/Relatorios/FormRelatorioCompra.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormRelatorioCompra));
             this.panel1 = new System.Windows.Forms.Panel();
             this.label7 = new System.Windows.Forms.Label();
@@ -50,9 +51,10 @@
             this.panel1.BackColor = System.Drawing.Color.Red;
             this.panel1.Controls.Add(this.label7);
             this.panel1.Controls.Add(this.label6);
-            this.panel1.Location = new System.Drawing.Point(-1, 344);
+            this.panel1.Location = new System.Drawing.Point(-2, 423);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(482, 23);
+            this.panel1.Size = new System.Drawing.Size(723, 28);
             this.panel1.TabIndex = 65;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
@@ -60,9 +62,11 @@
             // 
             this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(345, 4);
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(518, 5);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(134, 13);
+            this.label7.Size = new System.Drawing.Size(196, 16);
             this.label7.TabIndex = 1;
             this.label7.Text = "by Fernando Costa Colares";
             this.label7.Click += new System.EventHandler(this.label7_Click);
@@ -70,9 +74,11 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(3, 3);
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(4, 4);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.Size = new System.Drawing.Size(86, 16);
             this.label6.TabIndex = 0;
             this.label6.Text = "V 3.0 - 2024";
             this.label6.Click += new System.EventHandler(this.label6_Click);
@@ -85,19 +91,30 @@
             this.DGV_Compra.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.DGV_Compra.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.DGV_Compra.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGV_Compra.Location = new System.Drawing.Point(12, 72);
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DGV_Compra.DefaultCellStyle = dataGridViewCellStyle1;
+            this.DGV_Compra.Location = new System.Drawing.Point(18, 89);
+            this.DGV_Compra.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.DGV_Compra.Name = "DGV_Compra";
             this.DGV_Compra.ReadOnly = true;
             this.DGV_Compra.RowHeadersWidth = 51;
-            this.DGV_Compra.Size = new System.Drawing.Size(456, 247);
+            this.DGV_Compra.Size = new System.Drawing.Size(684, 304);
             this.DGV_Compra.TabIndex = 64;
             this.DGV_Compra.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGV_Compra_CellContentClick);
             // 
             // bt_gerar
             // 
-            this.bt_gerar.Location = new System.Drawing.Point(12, 43);
+            this.bt_gerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_gerar.Location = new System.Drawing.Point(18, 53);
+            this.bt_gerar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.bt_gerar.Name = "bt_gerar";
-            this.bt_gerar.Size = new System.Drawing.Size(459, 23);
+            this.bt_gerar.Size = new System.Drawing.Size(688, 28);
             this.bt_gerar.TabIndex = 63;
             this.bt_gerar.Text = "Gerar Relatório";
             this.bt_gerar.UseVisualStyleBackColor = true;
@@ -106,9 +123,11 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 23);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 28);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.Size = new System.Drawing.Size(27, 16);
             this.label2.TabIndex = 62;
             this.label2.Text = "De";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -116,34 +135,40 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(243, 23);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(364, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.Size = new System.Drawing.Size(29, 16);
             this.label1.TabIndex = 61;
             this.label1.Text = "até";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // dtp_fim
             // 
-            this.dtp_fim.Location = new System.Drawing.Point(271, 17);
+            this.dtp_fim.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtp_fim.Location = new System.Drawing.Point(406, 21);
+            this.dtp_fim.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dtp_fim.Name = "dtp_fim";
-            this.dtp_fim.Size = new System.Drawing.Size(200, 20);
+            this.dtp_fim.Size = new System.Drawing.Size(298, 22);
             this.dtp_fim.TabIndex = 60;
             this.dtp_fim.ValueChanged += new System.EventHandler(this.dtp_fim_ValueChanged);
             // 
             // dtp_inicio
             // 
-            this.dtp_inicio.Location = new System.Drawing.Point(37, 17);
+            this.dtp_inicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtp_inicio.Location = new System.Drawing.Point(56, 21);
+            this.dtp_inicio.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dtp_inicio.Name = "dtp_inicio";
-            this.dtp_inicio.Size = new System.Drawing.Size(200, 20);
+            this.dtp_inicio.Size = new System.Drawing.Size(298, 22);
             this.dtp_inicio.TabIndex = 59;
             this.dtp_inicio.ValueChanged += new System.EventHandler(this.dtp_inicio_ValueChanged);
             // 
             // FormRelatorioCompra
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(480, 363);
+            this.ClientSize = new System.Drawing.Size(720, 447);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.DGV_Compra);
             this.Controls.Add(this.bt_gerar);
@@ -151,7 +176,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dtp_fim);
             this.Controls.Add(this.dtp_inicio);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "FormRelatorioCompra";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
